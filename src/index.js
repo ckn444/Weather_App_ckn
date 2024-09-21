213b4e6eaa1864bad80ac4065f6607417ebf4679
ckn444 <cknewton@y7mail.com>
2024-09-21 12:47:59 +1000

    add details and update css for current details

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ function actionSubmit(event) {
 function getCityData(city) {
   let apiKey = "073da528da745of5bbcaae543e06t78e";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
-  axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(showWeather);
 }
 
-//Use the provided city data to populate city and current tempurature
-function showTemperature(response) {
-  console.log(response.data);
-  let currentCityTemp = Math.round(response.data.temperature.current);
-  console.log(`this is the current temp value ${currentCityTemp}`);
-  let currentCity = response.data.city;
+//Use the provided city data to populate city and current weather
+function showWeather(response) {
+  console.log(response.data); //test only
   let city = document.querySelector("#weather-app-city");
   let temp = document.querySelector("#current-temp");
-  city.innerHTML = currentCity;
-  temp.innerHTML = currentCityTemp;
+  let description = document.querySelector("#description");
+
+  city.innerHTML = response.data.city;
+  temp.innerHTML = Math.round(response.data.temperature.current);
+  description.innerHTML = response.data.condition.description;
 }
 
 //Process form submission
